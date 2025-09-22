fe45a2073d0cb6f4a663271eeb8f31f751243594
Elijah Friedman <efriedman810@gmail.com>
2025-09-21 20:45:14 -0400

    Rename installed binary to keypause

--- Formula/keypauseapp.rb
+++ Formula/keypauseapp.rb
@@ -1,12 +1,12 @@
-class Keypauseapp < Formula
+class Keypause < Formula
   desc "Pause/unpause keys on macOS (CLI tool)"
-  homepage "https://github.com/elijahfriedman/keypauseapp"
-  url "https://github.com/elijahfriedman/keypauseapp/releases/download/v1.0.0/keypauseapp"
+  homepage "https://github.com/elijahfriedman/keypause"
+  url "https://github.com/elijahfriedman/keypause/releases/download/v1.0.0/keypauseapp"
   sha256 "62549f54d66001a759a6bdce0a991449b39ba4ef0eac3cb913f5a51c3fed79a2"
   version "1.0.0"
 
   def install
-    bin.install "keypauseapp"
+    bin.install "keypauseapp" => "keypause"
   end
 end
 
