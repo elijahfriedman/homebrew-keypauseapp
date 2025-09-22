6f255463468942043de8f891de07933341a7ca99
Elijah Friedman <efriedman810@gmail.com>
2025-09-21 20:25:09 -0400

    Add keypauseapp v1.0.0 formula

---
+++ Formula/keypauseapp.rb
@@ -0,0 +1,12 @@
+class Keypauseapp < Formula
+  desc "Pause/unpause keys on macOS (CLI tool)"
+  homepage "https://github.com/elijahfriedman/keypauseapp"
+  url "https://github.com/elijahfriedman/keypauseapp/releases/download/v1.0.0/keypauseapp"
+  sha256 "62549f54d66001a759a6bdce0a991449b39ba4ef0eac3cb913f5a51c3fed79a2"
+  version "1.0.0"
+
+  def install
+    bin.install "keypauseapp"
+  end
+end
+
